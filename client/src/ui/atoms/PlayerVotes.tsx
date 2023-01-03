aca5a3ad60b76bd6eae1e1ba224fec34b06443b5
Richard Ng <richardcrng@gmail.com>
2023-01-03 21:16:49 +0000

    Edit PlayerVotes component

--- client/src/ui/atoms/PlayerVotes.tsx
+++ client/src/ui/atoms/PlayerVotes.tsx
@@ -1,25 +1,66 @@
+import styled from 'styled-components';
 import { Player } from "../../types/game.types";
 
 interface Props {
-  players: Record<string, Player>
+  className?: string;
+  players: Record<string, Player>;
 }
 
-export default function PlayerVotes({ players }: Props): JSX.Element {
+export default function PlayerVotes({ className, players }: Props): JSX.Element {
   return (
-    <div className="overflow-x-auto">
-      <table className="table table-compact w-full">
-        {Object.values(players).map(({ id, name, vote }) => {
-          const hasVoted = typeof vote === 'string'
-          
+    <div className={className}>
+      <VoteContainer>
+        {Object.values(players).map(({ id, name, vote }, idx) => {
+          const hasVoted = typeof vote === "string";
+
           return (
-            <tr>
-              <th>{name}</th>
-              <td>{hasVoted ? "🗳️" : "🤔"}</td>
-              <td>{hasVoted ? "VOTE CAST" : "THINKING"}</td>
-            </tr>
+            <PlayerVote
+              key={id}
+              {...{ hasVoted, name }}
+              rowStart={idx + 1}
+            />
           );
         })}
-      </table>
+      </VoteContainer>
     </div>
   );
 }
+
+const VoteContainer = styled.div.attrs({
+  className: 'grid gap-2'
+})`
+  grid-template-columns: repeat(3, min-content);
+`
+
+interface PlayerVoteProps {
+  hasVoted: boolean;
+  name: string;
+  rowStart: number;
+}
+
+const PlayerVote = ({ hasVoted, name, rowStart }: PlayerVoteProps) => (
+  <>
+    <Name {...{ rowStart }}>{name}</Name>
+    <Emoji {...{ rowStart }}>{hasVoted ? "🗳️" : "🤔"}</Emoji>
+    <Status {...{ rowStart }}>{hasVoted ? "vote cast" : "thinking"}</Status>
+  </>
+);
+
+const Name = styled.span.attrs({
+  className: 'font-bold'
+})`
+  grid-row-start: ${({ rowStart }: { rowStart: number }) => rowStart};
+  grid-column-start: 1;
+`
+
+const Emoji = styled.span`
+  grid-row-start: ${({ rowStart }: { rowStart: number }) => rowStart};
+  grid-column-start: 2;
+`;
+
+const Status = styled.span.attrs({
+  className: 'italic'
+})`
+  grid-row-start: ${({ rowStart }: { rowStart: number }) => rowStart};
+  grid-column-start: 3;
+`;
