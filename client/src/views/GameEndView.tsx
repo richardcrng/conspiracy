d75a89b3d2330f89142cdad5c6488d51753d842f
Richard Ng <richardcrng@gmail.com>
2023-01-04 08:49:16 +0000

    Remove unused imports

--- client/src/views/GameEndView.tsx
+++ client/src/views/GameEndView.tsx
@@ -1,8 +1,7 @@
 import { Game, Player } from "../types/game.types"
 import { PlayerGameEndHandlers } from "../types/handler.types";
 import PlayerGameEndView from "../ui/organisms/PlayerGameEndView";
-import PlayerOngoingView from "../ui/organisms/PlayerOngoingView";
-import { getConspiracyTargetName, getGameOutcome, getPlayerOutcome, isInnocent, isWinOutcome } from "../utils/game-utils";
+import { getGameOutcome, getPlayerOutcome, isWinOutcome } from "../utils/game-utils";
 
 interface Props extends PlayerGameEndHandlers {
   game: Game;
